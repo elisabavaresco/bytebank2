045793c1e764f6a313f8a9223f42993f299c649c
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2022-01-04 15:13:55 -0300

    adding image and contacts button

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,22 @@ class BytebankApp extends StatelessWidget {
         appBar: AppBar(
             title: Text('Dashboard')
         ),
+        body: Column(
+          children: <Widget>[
+            Image.asset('images/bytebank_logo.png'),
+            Container(
+              height: 120,
+              width: 100,
+              color: Colors.green,
+              child: Column(
+                children: <Widget>[
+                  Icon(Icons.people),
+                  Text('Contacts'),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
