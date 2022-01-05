e391602bedd7e16d303f4ec8e762bb8741b4f65a
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2022-01-04 22:33:48 -0300

    Remove unnecessary comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main () => runApp(BytebankApp());
+void main() => runApp(BytebankApp());
 
 class BytebankApp extends StatelessWidget {
   @override
@@ -14,9 +14,11 @@ class BytebankApp extends StatelessWidget {
         ),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>(Colors.blueAccent.shade700),
+            backgroundColor:
+                MaterialStateProperty.all<Color>(Colors.blueAccent.shade700),
           ),
-      )),
+        ),
+      ),
       home: Dashboard(),
     );
   }
@@ -29,8 +31,7 @@ class Dashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text('Dashboard'),
-//              backgroundColor: Colors.green[900],
+        title: Text('Dashboard'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -41,26 +42,24 @@ class Dashboard extends StatelessWidget {
             child: Image.asset('images/bytebank_logo.png'),
           ),
           Padding(
-            padding: const EdgeInsets.fromLTRB (16.0,16.0,16.0,24.0),
+            padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 24.0),
             child: Container(
               padding: EdgeInsets.all(8.0),
               height: 100,
               width: 150,
-//              color: Colors.green[900],
               color: Theme.of(context).primaryColor,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: const <Widget>[
-                  Icon(Icons.people,
+                  Icon(
+                    Icons.people,
                     size: 32.0,
                     color: Colors.white,
                   ),
-                  Text('Contacts',
-                    style: TextStyle(
-                        fontSize: 16.0,
-                        color: Colors.white
-                    ),
+                  Text(
+                    'Contacts',
+                    style: TextStyle(fontSize: 16.0, color: Colors.white),
                   ),
                 ],
               ),
