30cd9215441f1cbd6fdac87c2fcadebae8f2a714
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2022-01-05 15:43:16 -0300

    add contact form class

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank2/screens/contact_form.dart';
 import 'package:bytebank2/screens/contacts_list.dart';
 import 'package:bytebank2/screens/dashboard.dart';
 import 'package:flutter/material.dart';
@@ -21,7 +22,7 @@ class BytebankApp extends StatelessWidget {
           ),
         ),
       ),
-      home: Dashboard(),
+      home: ContactForm(),
     );
   }
 }
