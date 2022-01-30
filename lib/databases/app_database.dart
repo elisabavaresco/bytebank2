cdde2cc1fe49985c27b8fd6dc8e3dc9582732899
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2022-01-30 18:05:35 -0300

    clean database

--- lib/databases/app_database.dart
+++ lib/databases/app_database.dart
@@ -4,14 +4,16 @@ import 'package:sqflite/sqflite.dart';
 
 Future<Database> createDatabase() {
   return getDatabasesPath().then(
-    (dbPath) {
+        (dbPath) {
       final String path = join(dbPath, 'bytebank.db');
       return openDatabase(path, onCreate: (db, version) {
         db.execute('CREATE TABLE contacts ('
             'id INTEGER PRIMARY KEY,'
             'name TEXT, '
             'account_number INTEGER)');
-      }, version: 1);
+      }, version: 1,
+        onDowngrade: onDatabaseDowngradeDelete,
+      );
     },
   );
 }
